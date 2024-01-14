22671ae6980d140dbb72913e4ed2c84f5044a7b4
soumitsalman <soumitsr@gmail.com>
2024-01-13 20:18:34 -0500

    fixing app.py for making it run in azure in HTTP

--- app.py
+++ app.py
@@ -2,34 +2,24 @@
 import slack_receiver
 from flask import Flask, request
 from slack_bolt.adapter.flask import SlackRequestHandler
-from slack_bolt.adapter.socket_mode import SocketModeHandler
-import config
 
-# this can run on either SOCKET mode or HTTP mode
-# socket mode requires that in the slack app management portal "Socket Mode" is turned on
-MODE = "HTTP" # "SOCKET"
+# running in HTTP mode
+app = Flask(__name__)
+handler = SlackRequestHandler(slack_receiver.app)
 
+@app.route("/slack/events", methods=["POST"])
+def slack_events():
+    return handler.handle(request)
 
-# app = Flask(__name__)
-# handler = SlackRequestHandler(bolt_app)
-
-# @app.route("/slack/events", methods=["POST"])
-# def slack_events():
-#     return handler.handle(request)
-
-# start the app
 if __name__ == "__main__":
-    if MODE == "HTTP":
-        # HTTP mode is powered by flask
-        app = Flask(__name__)
-        handler = SlackRequestHandler(slack_receiver.app)
+    app.run(port=8000)
 
-        @app.route("/slack/events", methods=["POST"])
-        def slack_events():
-            return handler.handle(request)
-        
-        app.run(port=8000)
+# uncomment the following to run in SOCKET mode
+# import config
+# from slack_bolt.adapter.socket_mode import SocketModeHandler      
+# app = SocketModeHandler(slack_receiver.app, config.get_slack_app_token())
+# if __name__ == "__main__":
+#     app.start()
 
-    else: # MODE == "SOCKET"        
-        handler = SocketModeHandler(slack_receiver.app, config.get_slack_app_token())
-        handler.start()
+
+    
